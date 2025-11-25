bfb12a836cde237c504550f68455c37663ed81b6
Leslie Middleton <lesliemddltn@gmail.com>
2025-11-25 13:14:36 -0500

    Initial ISS overhead notifier script

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
+import os
+
 import requests
 from datetime import datetime
 import smtplib
 import time
+from dotenv import load_dotenv
+load_dotenv(override=True)
 
 MY_LAT = 35.496456 # Your latitude
 MY_LNG = -83.450469 # Your longitude
-MY_EMAIL = "youremail@gmail.com"
-PASSWORD = "yourpassword"
+MY_EMAIL = os.getenv("MY_EMAIL")
+PASSWORD = os.getenv("PASSWORD")
+SEND_TO = os.getenv("SEND_TO")
 
 def is_iss_overhead():
     # Location of International Space Station
@@ -45,8 +50,9 @@ def is_dark():
                       int(suntimes_data["results"]["sunset"].split("T")[1].split(":")[1]))
 
     time_now = set_hour(datetime.now().hour, datetime.now().minute)
-    if (sunrise < time_now < sunset
+    if time_now >= sunset or time_now <= sunrise:
             return True
+    return False
 
 while True:
     time.sleep(300)
@@ -55,6 +61,6 @@ while True:
                 connection.starttls()
                 connection.login(user=MY_EMAIL, password=PASSWORD)
                 connection.sendmail(from_addr=MY_EMAIL,
-                                    to_addrs="sendtoemail@gmail.com",
+                                    to_addrs=SEND_TO,
                                     msg=f"Subject: International Space Station is overhead!\n\n"
                                         f"Go outside and look up to see the International Space Station!")
